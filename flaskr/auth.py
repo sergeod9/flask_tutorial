6714a3bd358b9c3cbaea0c2ad249b4b34ac4bbc7
sergeod9 <georges.dahdouh@gmail.com>
2020-06-09 20:22:50 +0300

    Added views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,7 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth',__name__, url_prefix='/auth')
 
-@bp.route('/register',methods = ('GET', 'POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -18,19 +18,24 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
-        elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
-            error = 'User {} is already registered'.format(username)
-        
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',(username, generate_password_hash(password))
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
             )
             db.commit()
             return redirect(url_for('auth.login'))
+
         flash(error)
+
     return render_template('auth/register.html')
           
 @bp.route('/login', methods=('GET', 'POST'))
@@ -64,10 +69,10 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?',(user_id,)
-        ).fetchone
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
-@bp.rount('/logout')
+@bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
@@ -77,6 +82,8 @@ def login_required(view):
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
+
         return view(**kwargs)
+
     return wrapped_view
 
